7f757d77425221530c6cb642b3788baf3c34cb41
Joacir Junior <jr.joacir@gmail.com>
2018-08-07 23:04:07 -0300

    Implementado teste para o método require_all

--- spec/require_smasher_spec.rb
+++ spec/require_smasher_spec.rb
@@ -1,5 +1,50 @@
 require "spec_helper"
 
 RSpec.describe RequireSmasher do
+  context '#require_all' do
+    subject { require_all(require_1, require_2, require_3, require_4) }
 
+    context 'when required list is invalid' do
+      let(:require_1) { 'spec/fixtures/without_subdirectories/without_errors' }
+      let(:require_2) { 'rake' }
+      let(:require_3) { 'invalid_directory_1' }
+      let(:require_4) { 'invalid_gem_1' }
+
+      let(:error_message) { [error_message_1, error_message_2, error_message_3, error_message_4].to_s }
+      let(:error_message_1) { "Error requering gem '#{require_3}': cannot load such file -- #{require_3}" }
+      let(:error_message_2) { "Directory '#{require_3}' does not exist" }
+      let(:error_message_3) { "Error requering gem '#{require_4}': cannot load such file -- #{require_4}" }
+      let(:error_message_4) { "Directory '#{require_4}' does not exist" }
+
+      it 'raise a StandardError with message for gems and directories' do
+        expect{ subject }.to raise_error(StandardError, error_message)
+      end
+    end
+
+    context 'when required list is valid' do
+      let(:require_1) { 'spec/fixtures/without_subdirectories/without_errors' }
+      let(:require_2) { 'rake' }
+      let(:require_3) { 'spec/fixtures/with_subdirectories/without_errors' }
+      let(:require_4) { 'rspec' }
+
+      it 'return nil' do
+        expect(subject).to be_nil
+      end
+    end
+  end
+
+  context '#require_gem' do
+  end
+
+  context '#require_dir' do
+  end
+
+  context '#require_dirs' do
+  end
+
+  context '#require_gems' do
+  end
+
+  context '#require_gems' do
+  end
 end
