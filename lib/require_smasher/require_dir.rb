0113f80da8c807885b19b7f4c417967ba6b4d554
Joacir Junior <jr.joacir@gmail.com>
2018-08-16 23:27:39 -0300

    Refatorado módulo RequireDir para respeita regra do Rubocop para o tamanho de método

--- lib/require_smasher/require_dir.rb
+++ lib/require_smasher/require_dir.rb
@@ -1,7 +1,7 @@
 module RequireDir
   def self.req(directories)
     raise StandardError, 'Directory was not informed' if directories.empty?
-    require_files(files(directories))
+    require_files_list(files(directories))
   end
 
   def self.files(directories)
@@ -13,26 +13,30 @@ module RequireDir
     files
   end
 
-  def self.require_files(files, attempt = 0)
-    files_with_error = []
-    errors = []
+  def self.require_files_list(files, attempt = 0)
+    errors_list = require_files(files)
+
+    return if errors_list[:files_with_error].empty?
+    attempt += 1 if files == errors_list[:files_with_error]
+
+    raise StandardError, errors_list[:errors] if attempt > 1
+    require_files_list(errors_list[:files_with_error], attempt)
+  end
+
+  def self.require_files(files)
+    errors_list = { files_with_error: [], errors: [] }
 
     files.uniq.each do |file|
       begin
         require_relative file
       rescue StandardError => error
-        files_with_error << file
-        errors << "Error while requiring file #{file}: #{error.message}"
+        errors_list[:files_with_error] << file
+        errors_list[:errors] << "Error while requiring file #{file}: #{error.message}"
       end
     end
 
-    return if files_with_error.empty?
-
-    attempt += 1 if files == files_with_error
-
-    raise StandardError, errors if attempt > 1
-    require_files(files_with_error, attempt)
+    errors_list
   end
 
-  private_class_method :files, :require_files
+  private_class_method :files, :require_files, :require_files_list
 end
