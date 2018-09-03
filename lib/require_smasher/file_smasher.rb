ed67f772888f264c040b7b56003925f890d8a59a
Joacir Oliveira Santos Júnior <joacir.junior@vagas.com.br>
2018-09-03 08:53:21 -0300

    Removido chamado de método File.join, pois não havia necessidade

--- lib/require_smasher/file_smasher.rb
+++ lib/require_smasher/file_smasher.rb
@@ -4,7 +4,7 @@ module FileSmasher
   class << self
     def files_by(directory)
       raise StandardError, "Directory '#{directory}' does not exist" unless Dir.exist?(directory)
-      Dir.glob(File.join("./#{directory}", '**', '*.rb'))
+      Dir.glob("./#{directory}/**/*.rb")
     end
 
     def files(directories)
